4490b993fff74e6e5c5b1f94c7f60b0899fb44ce
bombomthon <josephploggi@gmail.com>
2025-02-25 22:05:37 -0500

    Do exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+
+function roundDecimal( num ) {
+    if ( typeof( num ) !== "number" ) { return( "ERROR" ); }
+    return( Math.round( num * 10 ) / 10 );
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function( temp ) {
+    if ( typeof( temp ) !== "number" ) { return( "ERROR" ); }
+    return( roundDecimal( (temp -32) * 5/9 ) );
+};
+
+const convertToFahrenheit = function( temp ) {
+    if ( typeof( temp ) !== "number" ) { return( "ERROR" ); }
+    return( roundDecimal( (temp* 9/5) + 32 ) );
 };
 
 // Do not edit below this line
