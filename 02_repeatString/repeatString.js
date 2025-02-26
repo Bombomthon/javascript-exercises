779a7b1b02e2f624941c68b44b011222db343f07
bombomthon <josephploggi@gmail.com>
2025-02-25 21:24:01 -0500

    Do exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function( stringInp, num ) {
+    
 
+    repeatingString = ""
+    for ( let i = 0; i < num; i++ ) {
+        repeatingString += stringInp;
+    }
+    if ( num < 0 ){ return( "ERROR" ); }
+    return( repeatingString );
 };
 
 // Do not edit below this line
