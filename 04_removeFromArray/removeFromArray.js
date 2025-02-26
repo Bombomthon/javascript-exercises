c905470b96bbc95e0dfbecb02aae3b89dd390045
bombomthon <josephploggi@gmail.com>
2025-02-25 21:38:15 -0500

    Do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function( arr, ...toRemove ) {
+    for (const arg of toRemove) {
+        arr = arr.filter( item => item !== arg );
+    }
+    return( arr );
 };
 
 // Do not edit below this line
