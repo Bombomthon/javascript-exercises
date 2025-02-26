4111b3025fb997d22930623b73de0da6596d69cc
bombomthon <josephploggi@gmail.com>
2025-02-25 21:49:51 -0500

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function( startNum, endNum ) {
 
+    if (typeof( startNum ) !== "number") { return( "ERROR" ); }
+    if !( Number.isInteger( startNum ) ) { return( "ERROR" ); }
+    if (typeof( endNum ) !== "number")   { return( "ERROR" ); }
+    if !( Number.isInteger( endNum ) )   { return( "ERROR" ); }
+    
+    let sumNum = 0;
+    let num = startNum;
+    
+    while ( num <= endNum ){
+        sumNum += num;
+	num++;
+    }
+    if ( startNum > endNum ){ return( "ERROR" ); }
+    return( sumNum );
 };
 
 // Do not edit below this line
